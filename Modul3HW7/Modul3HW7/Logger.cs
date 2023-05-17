f8a5974f489719fe1805ebc7f504b7754b4b5f76
Kyrylo Zolotarov <zlatashay@gmail.com>
2023-05-18 01:01:12 +0300

    done!

---
+++ Modul3HW7/Modul3HW7/Logger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3HW7
+{
+    internal class Logger
+    {
+        private readonly SemaphoreSlim semaphoreSlim;
+        private List<string> _loggRecords = new List<string>();
+        public Logger()
+        {
+            semaphoreSlim = new SemaphoreSlim(1, 1);
+        }
+
+        public delegate Task NeedBackup(List<string> messege);
+        public event NeedBackup AskBackup;
+        public async Task LoggingAsynk()
+        {
+            var n = File.ReadAllText("settings.txt");
+            var n1 = int.Parse(n);
+            for (int i = 0; i < 50; i++)
+            {
+                await semaphoreSlim.WaitAsync();
+                try
+                {
+                    await AddRec();
+                    if (_loggRecords.Count % n1 == 0)
+                    {
+                        await AskBackup.Invoke(_loggRecords);
+                    }
+
+                    await Task.Delay(200);
+                }
+                finally
+                {
+                    semaphoreSlim.Release();
+                }
+            }
+        }
+
+        private Task AddRec()
+        {
+            _loggRecords.Add($"{DateTime.Now.ToShortTimeString()}: message");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Modul3HW7/Modul3HW7/Starter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3HW7
+{
+    internal class Starter
+    {
+        private Logger _logger = new Logger();
+        public Starter()
+        {
+            _logger.AskBackup += StoreAsync;
+        }
+        public async Task StoreAsync(List<string> logg)
+        {
+            var directory = "Backup";
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            var fileExtention = ".txt";
+            var fileName = directory + "\\" + DateTime.Now.ToString("hh.mm.ss dd.MM.yyyy") + fileExtention;
+            await File.WriteAllLinesAsync(fileName, logg);
+        }
+        public async Task Run()
+        {
+            var temp = _logger.LoggingAsynk();
+            var temp2 = _logger.LoggingAsynk();
+            await Task.WhenAll(temp, temp2);
+        }
+    }
+}
