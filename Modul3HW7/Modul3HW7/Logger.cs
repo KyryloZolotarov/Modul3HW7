bffd99e89039c588fa45faf384eb65b433138bfd
Kyrylo Zolotarov <zlatashay@gmail.com>
2023-05-22 00:56:48 +0300

    some changes

--- Modul3HW7/Modul3HW7/Logger.cs
+++ Modul3HW7/Modul3HW7/Logger.cs
@@ -8,43 +8,25 @@ namespace Modul3HW7
 {
     internal class Logger
     {
-        private readonly SemaphoreSlim semaphoreSlim;
         private List<string> _loggRecords = new List<string>();
-        public Logger()
-        {
-            semaphoreSlim = new SemaphoreSlim(1, 1);
-        }
+        private int x = 0;
 
-        public delegate Task NeedBackup(List<string> messege);
+        public delegate Task NeedBackup(List<string> messege, int x);
         public event NeedBackup AskBackup;
-        public async Task LoggingAsynk()
-        {
-            var n = File.ReadAllText("settings.txt");
-            var n1 = int.Parse(n);
-            for (int i = 0; i < 50; i++)
-            {
-                await semaphoreSlim.WaitAsync();
-                try
-                {
-                    await AddRec();
-                    if (_loggRecords.Count % n1 == 0)
-                    {
-                        await AskBackup.Invoke(_loggRecords);
-                    }
 
-                    await Task.Delay(200);
-                }
-                finally
-                {
-                    semaphoreSlim.Release();
-                }
+        public async Task AddRec(int n)
+        {
+            _loggRecords.Add($"{DateTime.Now.ToShortTimeString()}: message");
+            if (_loggRecords.Count % n == 0)
+            {
+                x++;
+                await AskBackup.Invoke(_loggRecords, n * x);
             }
         }
 
-        private Task AddRec()
+        public List<string> GetLogs()
         {
-            _loggRecords.Add($"{DateTime.Now.ToShortTimeString()}: message");
-            return Task.CompletedTask;
+            return _loggRecords;
         }
     }
 }
--- Modul3HW7/Modul3HW7/Starter.cs
+++ Modul3HW7/Modul3HW7/Starter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Modul3HW7
@@ -9,26 +10,39 @@ namespace Modul3HW7
     internal class Starter
     {
         private Logger _logger = new Logger();
+        private int n = 0;
         public Starter()
         {
             _logger.AskBackup += StoreAsync;
+            n = int.Parse(File.ReadAllText("settings.txt"));
         }
-        public async Task StoreAsync(List<string> logg)
+
+        public async Task StoreAsync(List<string> logg, int endIndex)
         {
             var directory = "Backup";
             if (!Directory.Exists(directory))
             {
                 Directory.CreateDirectory(directory);
             }
+
             var fileExtention = ".txt";
-            var fileName = directory + "\\" + DateTime.Now.ToString("hh.mm.ss dd.MM.yyyy") + fileExtention;
-            await File.WriteAllLinesAsync(fileName, logg);
+            var fileName = directory + "\\" + DateTime.Now.ToString("hh.mm.ss.ffff dd.MM.yyyy") + fileExtention;
+            await File.WriteAllLinesAsync(fileName, logg.Take(endIndex));
         }
+
         public async Task Run()
         {
-            var temp = _logger.LoggingAsynk();
-            var temp2 = _logger.LoggingAsynk();
+            var temp = this.LoggingAsynс();
+            var temp2 = this.LoggingAsynс();
             await Task.WhenAll(temp, temp2);
+        }
+
+        public async Task LoggingAsynс()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                    await _logger.AddRec(n);
+            }
         }
     }
 }
